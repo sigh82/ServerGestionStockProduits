14e1c7d5102a566cdc2ca0ce18229c733ff136d8
sigh82 <sidatygh82@gmail.com>
2023-01-26 21:17:19 +0100

    first commit

---
+++ src/main/java/com/mr/formation/student/crud/productapi/ServerGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.mr.formation.student.crud.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitsApplication.class, args);
+	}
+
+}
