bfc107abc3e3dcac6b220ef6b3a916918a9f5322
sigh82 <sidatygh82@gmail.com>
2023-01-27 13:10:14 +0100

    first commit

---
+++ src/test/java/com/mr/formation/student/crud/productapi/ServerGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.formation.student.crud.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
