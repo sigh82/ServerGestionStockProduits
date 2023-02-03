4029d391038ef1810e80a5803b10147c8f9b5e3a
sigh82 <sidatygh82@gmail.com>
2023-02-03 13:25:05 +0100

    crud produits

---
+++ src/main/java/com/mr/formation/student/crud/productapi/entity/Produit.java
@@ -0,0 +1,68 @@
+package com.mr.formation.student.crud.productapi.entity;
+
+import java.util.Objects;
+
+public class Produit {
+	
+	private String ref;
+	
+	private int quantite;
+	
+	private float prixUnitaire;
+
+	public Produit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Produit(String ref, int quantite, float prixUnitaire) {
+		super();
+		this.ref = ref;
+		this.quantite = quantite;
+		this.prixUnitaire = prixUnitaire;
+	}
+
+	public String getRef() {
+		return ref;
+	}
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+
+	public float getPrixUnitaire() {
+		return prixUnitaire;
+	}
+
+	public void setPrixUnitaire(float prixUnitaire) {
+		this.prixUnitaire = prixUnitaire;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ref);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produit other = (Produit) obj;
+		return Objects.equals(ref, other.ref);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/mr/formation/student/crud/productapi/service/IProduitService.java
@@ -0,0 +1,17 @@
+package com.mr.formation.student.crud.productapi.service;
+
+import java.util.List;
+
+import com.mr.formation.student.crud.productapi.entity.Produit;
+
+public interface IProduitService {
+	
+	List<Produit> getProduits();
+	
+	void  addProduit(Produit produit);
+	
+	void updateProduit(Produit produit);
+	
+	void deleteProduit(String ref);
+
+}
